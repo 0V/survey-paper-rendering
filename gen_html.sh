ebac3f3e391b98abb0eccb28da61519e35e40d8b
G2X <g2simm@gmail.com>
2019-03-19 21:39:59 +0900

    Update slide

--- gen_html.sh
+++ gen_html.sh
@@ -27,7 +27,7 @@ cp $template_list $index_file
 
 for filepath in $ppt_files; do
     if [ -d "$filepath" ] ; then
-        echo "            <h2>  ${filepath##*/}  </h2>" >> ./index.html
+        echo "            <h2>${filepath##*/}</h2>" >> ./index.html
         echo "Group : ${filepath##*/}"
         find_files "${filepath}"
     fi
