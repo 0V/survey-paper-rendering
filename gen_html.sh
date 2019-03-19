219417521a5e1956a1bebbb618ff0da7758bc483
G2 <g2simm+bow@gmail.com>
2019-03-19 17:45:58 +0900

    Add auto generation system

--- gen_html.sh
+++ gen_html.sh
@@ -1,22 +1,45 @@
-echo "hoge"
+function find_files () {
+    target_dirpath="${1}/*"
+    for filepath in $target_dirpath; do
+        if [ -f $filepath ] ; then
+            FILE_NAME="${filepath##*/}"
+            FILE_WITH_FOLDER_NAME="${filepath#*ppts/}"
+            mkdir -p "./slides/${FILE_WITH_FOLDER_NAME%/*}"
+            sed -e "s/#{PLACEMENT_NAME}/${FILE_NAME%.*}/g" $template_html > ./slides/${FILE_WITH_FOLDER_NAME%.*}.html
+            echo "            <a href=\"./slides/${FILE_WITH_FOLDER_NAME%.*}.html\">  ${FILE_NAME%.*}  </a>" >> ./index.html
+            echo "      > ${FILE_NAME%.*}"
+        fi
+    done
+}
+
+echo "Start ..."
 
 files="./ppts/*"
-fileary=()
-dirary=()
+
+topfile="./index.html"
+template_list="./assets/temp/template_list.temp"
+template_html="./assets/temp/template.temp"
+
+cp $template_list $topfile
 
 for filepath in $files; do
-    if [ -f $filepath ] ; then
-        fileary+=("$filepath")
-    elif [ -d $filepath ] ; then
-        dirary+=("$filepath")
+    if [ -d $filepath ] ; then
+        echo "            <h2>  ${filepath##*/}  </h2>" >> ./index.html
+        echo "Group : ${filepath##*/}"
+        find_files $filepath
     fi
 done
 
-for FILE_PATH in ${fileary[@]}; do
-    FILE_NAME=${FILE_PATH##*/}
-    sed -e "s/#{PLACEMENT_NAME}/${FILE_NAME%.*}/g" ./template.html > ./slides/${FILE_NAME%.*}.html
-done
+# for FILE_PATH in ${fileary[@]}; do
+#     FILE_NAME=${FILE_PATH##*/}
+#     sed -e "s/#{PLACEMENT_NAME}/${FILE_NAME%.*}/g" ./template.html > ./slides/${FILE_NAME%.*}.html
+# done
+            
+cat <<EOS >> $topfile
+        </div>
+    </div>
+</body>
+</html>
+EOS
 
-for i in ${dirary[@]}; do
-    echo $i
-done
+echo "Finished"
