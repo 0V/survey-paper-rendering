0a581666fab0da16efa858093e509a776d24a3ee
G2X <g2simm@gmail.com>
2019-03-19 20:58:47 +0900

    Update slide

--- gen_html.sh
+++ gen_html.sh
@@ -1,36 +1,35 @@
 
+
+ppt_files="./ppts/*"
+index_file="./index.html"
+template_list="./assets/temp/template_list.temp"
+template_html="./assets/temp/template.temp"
+slide_dir="slides"
+
 function find_files () {
     local target_dirpath="${1}/*"
-    for filepath in $target_dirpath; do
-        if [ -f "$filepath" ] ; then
-            echo "A $filepath"
-            local FILE_NAME="${filepath##*/}"
-            local FILE_WITH_FOLDER_NAME="${filepath#*ppts/}"
+    for target_filepath in ${target_dirpath}; do
+        if [ -f "${target_filepath}" ] ; then
+            local FILE_NAME="${target_filepath##*/}"
+            local FILE_WITH_FOLDER_NAME="${target_filepath#*ppts/}"
             local FOLDER_NAME="${1##*/}"
             mkdir -p "./${slide_dir}/${FILE_WITH_FOLDER_NAME%/*}"
-            sed -e "s/#{PLACEMENT_NAME}/${FOLDER_NAME}\/${FILE_NAME%.*}/g" $template_html > ./${slide_dir}/${FILE_WITH_FOLDER_NAME%.*}.html
-            echo "            <a href=\"./${slide_dir}/${FILE_WITH_FOLDER_NAME%.*}.html\">  ${FILE_NAME%.*}  </a>" >> ./index.html
+            sed -e "s/#{PLACEMENT_NAME}/${FOLDER_NAME}\/${FILE_NAME%.*}/g" "${template_html}" > "./${slide_dir}/${FILE_WITH_FOLDER_NAME%.*}.html"
+            echo "            <a href=\"./${slide_dir}/${FILE_WITH_FOLDER_NAME%.*}.html\">  ${FILE_NAME%.*}  </a>" >> "${index_file}"
             echo "      > ${FILE_NAME%.*}"
         fi
     done
 }
 
-files="./ppts/*"
-topfile="./index.html"
-template_list="./assets/temp/template_list.temp"
-template_html="./assets/temp/template.temp"
-slide_dir="slides"
-
-
 echo "Start ..."
 
-cp $template_list $topfile
+cp $template_list $index_file
 
-for filepath in $files; do
-    if [ -d $filepath ] ; then
+for filepath in $ppt_files; do
+    if [ -d "$filepath" ] ; then
         echo "            <h2>  ${filepath##*/}  </h2>" >> ./index.html
         echo "Group : ${filepath##*/}"
-        find_files $filepath
+        find_files "${filepath}"
     fi
 done
 
@@ -39,7 +38,7 @@ done
 #     sed -e "s/#{PLACEMENT_NAME}/${FILE_NAME%.*}/g" ./template.html > ./slides/${FILE_NAME%.*}.html
 # done
             
-cat <<EOS >> $topfile
+cat <<EOS >> $index_file
         </div>
     </div>
 </body>
